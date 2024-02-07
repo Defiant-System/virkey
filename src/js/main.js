771d9d6c53131630f83a86556bd82e6ded064d16
Hakan Bilgin <hbi99@hotmail.com>
2024-02-07 22:23:57 +0100

    ui/ux

--- src/js/main.js
+++ src/js/main.js
@@ -12,9 +12,19 @@ const virkey = {
 		// DEV-ONLY-END
 	},
 	dispatch(event) {
+		let Self = virkey,
+			el;
 		switch (event.type) {
 			// system events
 			case "window.init":
+				break;
+			case "window.keydown":
+				window.find(`li[data-key="${event.keyCode}"]`).addClass("down");
+				if (event.keyCode === 13) window.find(`li[data-sub="${event.keyCode}"]`).addClass("down");
+				break;
+			case "window.keyup":
+				window.find(`li[data-key="${event.keyCode}"]`).removeClass("down");
+				if (event.keyCode === 13) window.find(`li[data-sub="${event.keyCode}"]`).removeClass("down");
 				break;
 			// custom events
 			case "open-help":
