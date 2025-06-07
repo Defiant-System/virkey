be54c9cfb4295a05bab9281f3b714997b6419d4c
Hakan Bilgin <hbi99@hotmail.com>
2025-06-07 20:39:48 +0200

    medch skin

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -1,6 +1,8 @@
 
 let Test = {
 	init(APP) {
+		APP.dispatch({ type: "set-keyboard-skin", arg: "mech" });
+		APP.dispatch({ type: "set-keyboard-mode", arg: "basic" });
 		// setTimeout(() => APP.dispatch({ type: "set-keyboard-skin", arg: "metal" }), 500);
 		// setTimeout(() => APP.dispatch({ type: "set-keyboard-mode", arg: "extended" }), 1500);
 	}
