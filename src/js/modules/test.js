2b1a611c42dbc95476407a113c6e7b66c28dbc40
Hakan Bilgin <hbi99@hotmail.com>
2025-06-07 22:30:06 +0200

    extended + numeric

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -2,7 +2,7 @@
 let Test = {
 	init(APP) {
 		APP.dispatch({ type: "set-keyboard-skin", arg: "mech" });
-		APP.dispatch({ type: "set-keyboard-mode", arg: "basic" });
+		APP.dispatch({ type: "set-keyboard-mode", arg: "full" });
 		// setTimeout(() => APP.dispatch({ type: "set-keyboard-skin", arg: "metal" }), 500);
 		// setTimeout(() => APP.dispatch({ type: "set-keyboard-mode", arg: "extended" }), 1500);
 	}
