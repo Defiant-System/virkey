9a825597af0d5b409158d7bcfe2f014c72819b00
Hakan Bilgin <hbi99@hotmail.com>
2025-06-08 22:04:19 +0200

    down state

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -3,6 +3,7 @@ let Test = {
 	init(APP) {
 		// APP.dispatch({ type: "set-keyboard-skin", arg: "mech" });
 		// APP.dispatch({ type: "set-keyboard-mode", arg: "full" });
+		// setTimeout(() => APP.content.find(`li[data-key="72"]`).addClass("down"), 100);
 		// setTimeout(() => APP.dispatch({ type: "set-keyboard-skin", arg: "metal" }), 500);
 		// setTimeout(() => APP.dispatch({ type: "set-keyboard-mode", arg: "extended" }), 1500);
 	}
